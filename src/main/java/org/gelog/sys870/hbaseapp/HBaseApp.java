7e3a5de0d6dc1606b2a4c5f1ad3f46d5048237a9
David Lauzon <davidonlaptop@gmail.com>
2015-10-04 18:42:44 -0400

    Refactored to clean up the code and split it into functions.

--- src/main/java/org/gelog/sys870/hbaseapp/HBaseApp.java
+++ src/main/java/org/gelog/sys870/hbaseapp/HBaseApp.java
@@ -1,15 +1,10 @@
 package org.gelog.sys870.hbaseapp;
 
-import java.io.Console;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
-import java.util.Scanner;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
@@ -17,10 +12,7 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.hadoop.hbase.client.Table;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,29 +24,75 @@ public class HBaseApp
 	
     public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
-        
         new HBaseApp();
     }
-    
     
     
     public HBaseApp() throws IOException
     {   
 		Configuration		conf;
 		Connection			conn;
-    	Admin				admin;
-    	String				tableName;
-    	TableName			tableNameH;  // TableName used by HBase (bytes ?)
-    	HTableDescriptor	table;
-    	String				family;
-    	InputStream			confStream;
+    	String				zkConnectionString;
     	
 //    	Scanner reader = new Scanner(System.in);  // Reading from System.in
 //    	System.out.println("Enter a number: ");
 //    	int n = reader.nextInt();
     	
     	System.out.println("Setting up HBase configuration ...");
+    	conf = configureHBase();
+        System.out.println("\t" + getPropertyTraceability(conf, "hbase.zookeeper.quorum") );
+        System.out.println("\t" + getPropertyTraceability(conf, "hbase.zookeeper.property.clientPort") );
+
+        
+        // Note: Verify that the client can connect to ZooKeeper (normally not required)
+        System.out.println("Connecting manually to ZooKeeper (not required) ...");
+        zkConnectionString = conf.get( "hbase.zookeeper.quorum" );
+        testingZooKeeper( zkConnectionString );
+        
+        
+        //System.exit(1);
+        
+        System.out.println("Using HBase client to connect to the ZooKeeper Quorum ...");
+        conn	= ConnectionFactory.createConnection( conf );
+    	ScanningMetaTable( conn );
+        
+        System.exit(1);
+    }
+    
+    
+    public void createTable( Connection conn ) throws IOException
+    {
+    	Admin				admin;
+    	String				tableName;
+    	TableName			tableNameH;  // TableName used by HBase (bytes ?)
+    	HTableDescriptor	table;
+    	String				family;
+    	
+    	admin		= conn.getAdmin();
+    	
+    	
+    	tableName	= "demo-table";
+    	tableNameH	= TableName.valueOf( tableName );
+		
+    	if ( admin.tableExists(tableNameH) ) {
+    		System.out.println("Table already exists. Deleting table " + tableName);
+    		admin.disableTable( tableNameH );
+    		admin.deleteTable( tableNameH );
+    	}
+    	
+    	System.out.println("Creating table " + tableName);
+    	family		= "cf";
+    	table		= new HTableDescriptor( tableNameH );
+    	table.addFamily( new HColumnDescriptor( family ) );
+    	admin.createTable( table );
+    }
+    
+    
+    public Configuration configureHBase()
+    {
+    	Configuration		conf;
+    	InputStream			confStream;
+    	
     	conf		= HBaseConfiguration.create();
     	confStream  = conf.getConfResourceAsInputStream("hello.xml");
         int available = 0;
@@ -73,59 +111,11 @@ public class HBaseApp
             conf.addResource("hdfs-site.xml");
         }
 
-        
-        ///conf.set("hbase.zookeeper.quorum", "localhost", "david");
-        
-        System.out.println("Connecting to HBase ZooKeeper Quorum ...");
-        System.out.println("\t" + getPropertyTraceability(conf, "hbase.zookeeper.quorum") );
-        System.out.println("\t" + getPropertyTraceability(conf, "hbase.zookeeper.property.clientPort") );
-    	
-        ZooKeeperWrapper	zk;
-        String				zkConnectionString;
-        int					zkSessionTimeout;
-        
-        zkConnectionString	= "192.168.99.100";
-        zkSessionTimeout	= 3000;
-        zk					= new ZooKeeperWrapper( zkConnectionString, zkSessionTimeout );
-        
-        System.out.println("Listing paths in ZooKeeper recursively ...");
-        zk.list( "/" );
-        
-        zk.disconnect();
-        //System.exit(1);
-        
-        
-        conn		= ConnectionFactory.createConnection( conf );
-    	
-        tableNameH = TableName.valueOf("hbase:meta");
-        
-        
-        
-        
-        System.exit(1);
-        
-        
-        admin		= conn.getAdmin();
-    	
-    	
-    	tableName	= "demo-table";
-    	tableNameH	= TableName.valueOf( tableName );
-		
-    	if ( admin.tableExists(tableNameH) ) {
-    		System.out.println("Table already exists. Deleting table " + tableName);
-    		admin.disableTable( tableNameH );
-    		admin.deleteTable( tableNameH );
-    	}
-    	
-    	System.out.println("Creating table " + tableName);
-    	family		= "cf";
-    	table		= new HTableDescriptor( tableNameH );
-    	table.addFamily( new HColumnDescriptor( family ) );
-    	admin.createTable( table );
-    	
 		// Add any necessary configuration files (hbase-site.xml, core-site.xml)
 		//config.addResource(new Path(System.getenv("HBASE_CONF_DIR"), "hbase-site.xml"));
 		//config.addResource(new Path(System.getenv("HADOOP_CONF_DIR"), "core-site.xml"));
+        
+        return conf;
     }
     
     
@@ -141,5 +131,33 @@ public class HBaseApp
         source	= (sources != null  ?  sources[sources.length-1]  :  "");
         
         return key + " = " + value + " (" + source + ")";
+    }
+    
+    
+    public void testingZooKeeper( String zkConnectionString ) throws IOException
+    {
+    	ZooKeeperWrapper	zk;
+        int					zkSessionTimeout;
+        
+        zkSessionTimeout	= 3000;
+        zk					= new ZooKeeperWrapper( zkConnectionString, zkSessionTimeout );
+        
+        System.out.println("Listing paths in ZooKeeper recursively ...");
+        zk.list( "/" );
+        
+        zk.disconnect();
+    }
+    
+    
+    public void ScanningMetaTable( Connection connection ) throws IOException
+    {
+    	String		tableName;
+    	TableName	tableNameH;  // TableName used by HBase (bytes ?)
+    	Table		table;
+    	
+    	tableName	= "hbase:meta";
+    	tableNameH	= TableName.valueOf( tableName );
+    	table		= connection.getTable( tableNameH );
+    	connection.getRegionLocator(tableNameH).getRegionLocation("row1".getBytes()).getHostname();
     }
 }
